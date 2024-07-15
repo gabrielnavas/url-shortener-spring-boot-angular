f9e7c5c36e7ce1a520fdebd5f15c43b6cc3546b6
navas <gabrielnavas@email.com>
2024-07-14 23:43:50 -0300

    add spinner to waiting page

--- url-shortener-ui/src/app/app.module.ts
+++ url-shortener-ui/src/app/app.module.ts
@@ -7,7 +7,9 @@ import {provideAnimationsAsync} from '@angular/platform-browser/animations/async
 import {HeaderComponent} from "./components/header/header.component";
 import {HttpClientModule} from "@angular/common/http";
 import {ClipboardModule} from "@angular/cdk/clipboard";
-import { UrlResolverComponent } from './pages/url-resolver/url-resolver.component';
+import {UrlResolverComponent} from './pages/url-resolver/url-resolver.component';
+import {MatCard, MatCardContent} from "@angular/material/card";
+import {MatProgressSpinner} from "@angular/material/progress-spinner";
 
 @NgModule({
   declarations: [
@@ -19,7 +21,10 @@ import { UrlResolverComponent } from './pages/url-resolver/url-resolver.componen
     AppRoutingModule,
     HeaderComponent,
     HttpClientModule,
-    ClipboardModule
+    ClipboardModule,
+    MatCard,
+    MatCardContent,
+    MatProgressSpinner
   ],
   providers: [
     provideAnimationsAsync()
